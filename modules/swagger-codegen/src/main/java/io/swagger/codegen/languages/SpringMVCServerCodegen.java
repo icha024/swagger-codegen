20a5216727b1aac54c45c65c6a6d20b625ddc5b9
Ian Chan <icha024@gmail.com>
2015-12-19 23:03:59 +1300

    New Spring-MVC config "asyncIface": Uses async servlet and default iface
    This template is mainly for Maven code-gen plugin use-case. When Swagger
    spec on existing project changed there is no need to manually copy/paste
    the new functions from the generated client. This will provide a default
    (empty) implementation to existing impl and user just need to override
    it.
    
    Because it is an interface, an actual implementation will be needed to
    actuate a service end-point. The new option will also enable async
    servlet.

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringMVCServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/SpringMVCServerCodegen.java
@@ -18,12 +18,13 @@ public class SpringMVCServerCodegen extends JavaClientCodegen implements Codegen
     public static final String CONFIG_PACKAGE = "configPackage";
     protected String title = "Petstore Server";
     protected String configPackage = "";
+    protected String templateFileName = "api.mustache";
 
     public SpringMVCServerCodegen() {
         super();
         outputFolder = "generated-code/javaSpringMVC";
         modelTemplateFiles.put("model.mustache", ".java");
-        apiTemplateFiles.put("api.mustache", ".java");
+        apiTemplateFiles.put(this.templateFileName, ".java");
         embeddedTemplateDir = templateDir = "JavaSpringMVC";
         apiPackage = "io.swagger.api";
         modelPackage = "io.swagger.model";
@@ -51,6 +52,13 @@ public class SpringMVCServerCodegen extends JavaClientCodegen implements Codegen
         );
 
         cliOptions.add(new CliOption(CONFIG_PACKAGE, "configuration package for generated code"));
+        supportedLibraries.put(DEFAULT_LIBRARY, "Default Spring MVC server stub.");
+        supportedLibraries.put("asyncIface", "Use async servlet feature and Java 8's default interface. Generating interface with service " +
+                "declaration is useful when using Maven plugin. Provide a implementation with @Controller to instantiate service.");
+        CliOption library = new CliOption(CodegenConstants.LIBRARY, "library template (sub-template) to use");
+        library.setDefault(DEFAULT_LIBRARY);
+        library.setEnum(supportedLibraries);
+        cliOptions.add(library);
     }
 
     public CodegenType getTag() {
@@ -168,6 +176,12 @@ public class SpringMVCServerCodegen extends JavaClientCodegen implements Codegen
                 }
             }
         }
+        if("asyncIface".equals(getLibrary())) {
+            apiTemplateFiles.remove(this.templateFileName);
+            this.templateFileName = "api-asyncIface.mustache";
+            apiTemplateFiles.put(this.templateFileName, ".java");
+        }
+
         return objs;
     }
 
